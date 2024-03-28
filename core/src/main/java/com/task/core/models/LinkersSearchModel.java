f5bd456282bd20d53ccca208db41d0cd337e4eda
Aliaksandr Hvozdzeu <aliaksandr.hvozdzeu@gmail.com>
2024-03-28 10:07:12 +0200

    Refactoring

--- core/src/main/java/com/task/core/models/LinkersSearchModel.java
+++ core/src/main/java/com/task/core/models/LinkersSearchModel.java
@@ -1,7 +1,76 @@
 package com.task.core.models;
 
-import com.adobe.cq.export.json.ComponentExporter;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
 
-public interface LinkersSearchModel extends ComponentExporter {
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
+@Model(
+        adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class LinkersSearchModel {
+
+    @Inject
+    private int countPaginationRows;
+
+    @Inject
+    private String foundMessage;
+
+    @Inject
+    private String errorMessageHexColor;
+
+    @Inject
+    private String backgroundHexColor;
+
+    @Inject
+    private String buttonBackgroundHexColor;
+
+    @Inject
+    private String buttonTextHexColor;
+
+    @Inject
+    private String buttonUnavailableBackgroundHexColor;
+
+    @Inject
+    private String buttonUnavailableTextHexColor;
+
+    @PostConstruct
+    protected void init() {
+
+    }
+
+    public int getCountPaginationRows() {
+        return countPaginationRows;
+    }
+
+    public String getFoundMessage() {
+        return foundMessage;
+    }
+
+    public String getErrorMessageHexColor() {
+        return errorMessageHexColor;
+    }
+
+    public String getBackgroundHexColor() {
+        return backgroundHexColor;
+    }
+
+    public String getButtonBackgroundHexColor() {
+        return buttonBackgroundHexColor;
+    }
+
+    public String getButtonTextHexColor() {
+        return buttonTextHexColor;
+    }
+
+    public String getButtonUnavailableBackgroundHexColor() {
+        return buttonUnavailableBackgroundHexColor;
+    }
+
+    public String getButtonUnavailableTextHexColor() {
+        return buttonUnavailableTextHexColor;
+    }
 }
