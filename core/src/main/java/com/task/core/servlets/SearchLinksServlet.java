db79fda5d0c69e8ee37013ff544dbb0925576304
Aliaksandr Hvozdzeu <aliaksandr.hvozdzeu@gmail.com>
2024-03-28 18:04:03 +0200

    Add servlet logics

--- core/src/main/java/com/task/core/servlets/SearchLinksServlet.java
+++ core/src/main/java/com/task/core/servlets/SearchLinksServlet.java
@@ -1,34 +1,83 @@
 package com.task.core.servlets;
 
-import com.day.cq.commons.jcr.JcrConstants;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.json.JSONArray;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 
+import javax.jcr.*;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
-@Component(
-        immediate = true,
-        service = {Servlet.class},
-        property = {
-                Constants.SERVICE_DESCRIPTION + "=Search Links Servlet",
-                "sling.servlet.methods=" + HttpConstants.METHOD_GET,
-                "sling.servlet.paths=" + "/bin/searchlinks"
-        }
-)
+@Component(immediate = true, service = {Servlet.class}, property = {Constants.SERVICE_DESCRIPTION + "=Search Links Servlet", "sling.servlet.methods=" + HttpConstants.METHOD_GET, "sling.servlet.paths=" + "/bin/searchlinks"})
 public class SearchLinksServlet extends SlingSafeMethodsServlet {
 
+    private static final long serialVersionUID = 1L;
+
     @Override
-    protected void doGet(final SlingHttpServletRequest req,
-            final SlingHttpServletResponse resp) throws ServletException, IOException {
-        final Resource resource = req.getResource();
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Title = " + resource.getValueMap().get(JcrConstants.JCR_TITLE));
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        String contentPath = request.getParameter("path");
+        String linkToFind = request.getParameter("link");
+        if (StringUtils.isBlank(contentPath) || StringUtils.isBlank(linkToFind)) {
+            response.setStatus(SlingHttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("Content path or link parameter is missing");
+            return;
+        }
+
+        try {
+            List<String> nodesWithLink = findNodesWithLink(contentPath, linkToFind, request.getResourceResolver());
+            List<String> pathList = new ArrayList<>(nodesWithLink);
+            Type listType = new TypeToken<List<String>>() {}.getType();
+            Gson gson = new Gson();
+            String json = gson.toJson(pathList, listType);
+            response.setContentType("application/json");
+            response.getWriter().write(json);
+        } catch (Exception e) {
+            response.setStatus(SlingHttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Error occurred: " + e.getMessage());
+        }
+    }
+
+    private List<String> findNodesWithLink(String path, String linkToFind, ResourceResolver resolver) throws RepositoryException {
+        List<String> nodesWithLink = new ArrayList<>();
+        Session session = resolver.adaptTo(Session.class);
+        if (session != null) {
+            Node rootNode = session.getNode(path);
+            findNodesWithLinkRecursive(rootNode, linkToFind, nodesWithLink);
+        }
+        return nodesWithLink;
+    }
+
+    private void findNodesWithLinkRecursive(Node node, String linkToFind, List<String> nodesWithLink) throws RepositoryException {
+        PropertyIterator properties = node.getProperties();
+        while (properties.hasNext()) {
+            Property property = properties.nextProperty();
+            if (!property.isMultiple()) {
+                if (property.getType() == PropertyType.STRING) {
+                    String stringValue = String.valueOf(property.getValue());
+                    if (StringUtils.contains(stringValue, linkToFind)) {
+                        nodesWithLink.add(node.getPath());
+                    }
+                }
+            }
+        }
+
+        // Recursively traverse child nodes
+        NodeIterator childNodes = node.getNodes();
+        while (childNodes.hasNext()) {
+            Node childNode = childNodes.nextNode();
+            findNodesWithLinkRecursive(childNode, linkToFind, nodesWithLink);
+        }
     }
 }
--- ui.apps/src/main/content/jcr_root/apps/task/components/linkerssearchv1/clientlibs/js/script.js
+++ ui.apps/src/main/content/jcr_root/apps/task/components/linkerssearchv1/clientlibs/js/script.js
@@ -1,33 +1,48 @@
-function sendRequest() {
-
-    //let hiddenCountRow = $('#hiddenCountRow').val();
-
-    $('td', 'table').each(function(i) {
-        $(this).text(i+1);
+function sendRequest(path) {
+    $.ajax({
+        type: 'GET',
+        url: '/bin/searchlinks',
+        data: {
+            'path': path
+        },
+        success: function (response) {
+            console.log(response);
+            displayResults(response);
+        },
+        error: function (xhr, status, error) {
+            console.error(error);
+        }
     });
+}
 
-    $('table.paginated').each(function() {
-        var currentPage = 0;
+function displayResults(results) {
+    var $table = $('#paginatedTable');
+    $table.empty(); // Clear existing content
+    $table.append('<thead><tr><th scope="col">Link URL</th><th scope="col">Node path</th></tr></thead>');
+
+    if (results && results.length > 0) {
+        var numRows = results.length;
         var numPerPage = 10;
-        var $table = $(this);
-        $table.bind('repaginate', function() {
-            $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-        });
-        $table.trigger('repaginate');
-        var numRows = $table.find('tbody tr').length;
         var numPages = Math.ceil(numRows / numPerPage);
+        var currentPage = 0;
+
+        for (var i = 0; i < numRows; i++) {
+            var result = results[i];
+            $table.append('<tr><td>' + result.link + '</td><td>' + result.pagePath + '</td></tr>');
+        }
+
+        $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+
         var $pager = $('<div class="pager"></div>');
         for (var page = 0; page < numPages; page++) {
-            $('<span class="page-number"></span>').text(page + 1).bind('click', {
-                newPage: page
-            }, function(event) {
+            $('<span class="page-number"></span>').text(page + 1).bind('click', { newPage: page }, function (event) {
                 currentPage = event.data['newPage'];
-                $table.trigger('repaginate');
+                $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
                 $(this).addClass('active').siblings().removeClass('active');
             }).appendTo($pager).addClass('clickable');
         }
         $pager.insertBefore($table).find('span.page-number:first').addClass('active');
-    });
-
-
+    } else {
+        $table.append('<tbody><tr><td colspan="2">No results found</td></tr></tbody>');
+    }
 }
